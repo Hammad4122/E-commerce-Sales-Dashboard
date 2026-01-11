0d9c9a3836cd69a493c904c35dbd3c4135776788
Hammad4122 <muhammadhammadateeq@gmail.com>
2026-01-12 02:02:04 +0500

    Fixed Path

--- APP/modules/read_csv.py
+++ APP/modules/read_csv.py
@@ -2,5 +2,5 @@ import pandas as pd
 import streamlit as st
 
 def read_csv():
-    df = pd.read_csv(r"C:\Coding\Coding_Journey\Python Journey\Data_Visualization_Projects\E-commerce_Sales_Dashboard\Dataset\Sales_Data.csv",encoding="ISO-8859-1")
+    df = pd.read_csv("Dataset\Sales_Data.csv")
     return df
