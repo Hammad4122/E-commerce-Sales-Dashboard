70974db44701b9f8cb2364f067abcebaa9717e65
Hammad4122 <muhammadhammadateeq@gmail.com>
2026-01-12 02:08:04 +0500

    Fix

--- APP/modules/read_csv.py
+++ APP/modules/read_csv.py
@@ -2,7 +2,6 @@ import os
 import pandas as pd
 
 def read_csv():
-    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    base_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
     csv_path = os.path.join(base_dir, "Dataset", "Sales_Data.csv")
-    df = pd.read_csv(csv_path)
-    return df
+    return pd.read_csv(csv_path)
