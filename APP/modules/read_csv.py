0f8274587d33c0e32a37f5514984cd2decfef444
Hammad4122 <muhammadhammadateeq@gmail.com>
2026-01-12 02:03:30 +0500

    Fix

--- APP/modules/read_csv.py
+++ APP/modules/read_csv.py
@@ -2,5 +2,5 @@ import pandas as pd
 import streamlit as st
 
 def read_csv():
-    df = pd.read_csv("Dataset\Sales_Data.csv")
+    df = pd.read_csv("Dataset\Sales_Data.csv",encoding="ISO-8859-1")
     return df
