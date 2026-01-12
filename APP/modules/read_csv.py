6bff32bc374e7b4752d63bc543d2cd642a51af10
Hammad4122 <muhammadhammadateeq@gmail.com>
2026-01-12 11:24:39 +0500

    Fixed CSS

--- APP/modules/read_csv.py
+++ APP/modules/read_csv.py
@@ -1,7 +1,11 @@
 import os
 import pandas as pd
+import streamlit as st
 
 def read_csv():
-    base_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-    csv_path = os.path.join(base_dir, "Dataset", "Sales_Data.csv")
-    return pd.read_csv(csv_path)
+    try:
+        base_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+        csv_path = os.path.join(base_dir, "Dataset", "Sales_Data.csv")
+        return pd.read_csv(csv_path)
+    except :
+        st.error("Couldn't load the data. Try refreshing the page.")
